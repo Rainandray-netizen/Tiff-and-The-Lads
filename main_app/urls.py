f21f2fe285a53bac61627161236f57bb220b9c4f
Tiffany Bouchard <tiffany.bouchard@mail.utoronto.ca>
2020-07-08 21:45:53 -0400

    Update delete url

--- main_app/urls.py
+++ main_app/urls.py
@@ -17,8 +17,8 @@ urlpatterns = [
     # path('activity/<int:pk>/update/', views.ActivityUpdate.as_view(), name='activity_update'),
     
 
-    # path('routine/create/', views.RoutineCreate, name='routine_create'), ## Needs to be reviewed for drag and drop feature
+    path('routine/create/', views.routine_create, name='routine_create'), ## Needs to be reviewed for drag and drop feature
     # path('routine/<int:pk>/update/', views.RoutineUpdate.as_view(), name='routine_update'),
-    # path('routine/<int:pk>/delete/', views.RoutineDelete.as_view(), name='routine_delete'),
+    path('routine/delete/<int:id>', views.routine_delete, name='routine_delete'),
 ]    
     # path('activities/<int:pk>/update/', views.  , name=)
