28367869f152dde82b3b395567d5b7e63653c705
daronefrancis <daronefrancis@gmail.com>
2020-07-09 03:16:14 -0400

    added risk calculator when adding an  activity, added default value to Activity model risk_level

--- main_app/models.py
+++ main_app/models.py
@@ -46,6 +46,9 @@ class Activity(models.Model):
         choices=INTERACTION
     ) 
     profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    risk_level = models.IntegerField(
+        default=0
+    )
 
 
     def __str__(self):
@@ -55,5 +58,7 @@ class Routine(models.Model):
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
     date = models.DateField(default=datetime.date.today)
 
+    class Meta:
+        ordering = ['date']
     # def clean_activity(self):
     #     return self.cleaned_data['activity'].title()
